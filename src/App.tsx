2279e8c51a8392bf00b12f78b56f2cd82f025a67
kex5n <kentaro.ishii0407@gmail.com>
2022-06-14 01:58:48 +0900

    tmp

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Thread } from './models/thread'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <h1>お前とひろゆきの5ch</h1>
+        </header>
+        <main>
+          <h2>ひろゆきだけど質問ある？</h2>
+            <Thread />
+        </main>
+      </div>
+    </React.StrictMode>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Thread } from './models/thread';
+import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <div>
-      <header>
-        <h1>お前とひろゆきの5ch</h1>
-      </header>
-      <main>
-        <h2>ひろゆきだけど質問ある？</h2>
-          <Thread />
-      </main>
-    </div>
-  </React.StrictMode>
-);
+root.render(<App />);
